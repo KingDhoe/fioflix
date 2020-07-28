8a9e77688592732a31f4b2717b1ffda5c7125f79
KingDhoe <williammartins19@outlook.com>
2020-07-28 19:40:19 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -7,9 +7,41 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
-      
+
+      <BannerMain
+        videoTitle={dados_iniciais.categorias[0].videos[0].titulo}
+        url={dados_iniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dados_iniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dados_iniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dados_iniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dados_iniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dados_iniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dados_iniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
