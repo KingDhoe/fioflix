287f190059e58ff4c1a71067477afa378c7dbb11
KingDhoe <williammartins19@outlook.com>
2020-08-02 11:36:23 -0300

    The End

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,91 +1,90 @@
-import React, { useEffect, useState } from 'react';
+import React,{useState,useEffect} from 'react';
+import PageDefault from '../../../components/PageDefault'
 import { Link, useHistory } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
 import useForm from '../../../hooks/useForm';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 import videosRepository from '../../../repositories/videos';
 import categoriasRepository from '../../../repositories/categorias';
 
-function CadastroVideo(){
+
+
+function CadastroVideo() {
   const history = useHistory();
-  const [categorias, setCategorias] = useState();
+  const [categorias,setcCategorias] = useState([]);
   const categoryTitles = categorias.map(({ titulo }) => titulo);
-  const { HandlerChange, values } = useForm({
-    titulo: 'Video padrão',
-    url: 'https://www.youtube.com/watch?v=0XGqmGuqKKs',
-    categoria: 'Brasil',
+
+  const { HandleChange, values } = useForm({
+    titulo: '',
+    url: '',
+    categoria: '',
   });
 
   useEffect(() => {
-    categoriasRepository
-    .getAll()
-    .then((categoriasFormServer) => { 
-      setCategorias(categoriasFormServer);
+    categoriasRepository.getAll().then((categoriasFromServer)=>{
+      setcCategorias(categoriasFromServer);
     });
-  }, []);
+  },[]);
 
-    return (
-      <PageDefault>
-        <h1>Cadastro de Vídeo</h1>
+  return (
+    <PageDefault>
 
-        <form onSubmit={(event) =>{
-          event.preventDefault();
-            //alert('Vídeo Cadastrado com sucesso!');
+      <h1>Cadastro de Video</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
 
-            const categoriaEscolhida = categorias.find((categoria) => {
-              return categoria.titulo === values.categoria;
-            });
-            console.log('categoriaEscolhida', categoriaEscolhida);
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === values.categoria;
+        });
+        console.log('categoria Escolhida',categoriaEscolhida);
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: categoriaEscolhida.id,
+        }).then(()=>{
+          history.push('/');
+        });
 
-            videosRepository.create({
-              titulo: values.titulo,
-              url: values.url,
-              categoriaId: categoriaEscolhida.id,
-            })
-              .then(() => {
-                console.log('Cadastrou com sucesso!');
-                history.push('/');
-              });
-              
-            
-        }}>
-          <FormField
-            label="Titulo do Video"
-            value={values.titulo}
-            name="titulo"
-            onChange={HandlerChange}
-            required
-          />
+        
+      }}>
 
-          <FormField
-            label="URL"
-            value={values.url}
-            name="url"
-            onChange={HandlerChange}
-            required
-          />
+        <FormField
+          label="Titulo do video"
+          name="titulo"
+          type="text"
+          value={values.titulo}
+          onChange={HandleChange}
+          placeholder="Vídeo Padrão"
+        />
 
-          <FormField
-            label="Campeonato"
-            value={values.categoria}
-            name="categoria"
-            onChange={HandlerChange}
-            suggestions={categoryTitles}
-            required
-          />
+        <FormField
+          label="URL do video"
+          name="url"
+          type="text"
+          value={values.url}
+          onChange={HandleChange}
+          placeholder="https://www.youtube.com/watch?v=5qPuKhBa7Ag"
+        />
 
-          <Button type="submit">
-            Cadastrar
-          </Button>
-        </form>
+        <FormField
+          label="Categoria"
+          name="categoria"
+          value={values.categoria}
+          onChange={HandleChange}
+          suggestions={categoryTitles}
+          placeholder="Brasil"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
 
 
-        <Link to="/cadastro/categoria">
-            Cadastrar Campeonato
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
         </Link>
-      </PageDefault>
-    );
-  }
+    </PageDefault>
+  )
+}
 
-  export default CadastroVideo;
+
+export default CadastroVideo;
